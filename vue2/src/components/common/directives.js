aa5a4119120077f15a2e97107f2266c0708ab0e0
aa1479474496 <1479474496@qq.com>
2017-03-28 10:58:29 +0800

    自定义指令

--- vue2/src/components/common/directives.js
+++ vue2/src/components/common/directives.js
@@ -4,6 +4,8 @@ Vue.directive('focus', {
   // 当绑定元素插入到 DOM 中。
   bind: function(el, binding) {
     el.style.borderColor = binding.value;
+    console.log(binding.arg)
+    console.log(this)
   },
   inserted: function (el) {
     // 聚焦元素
